e9b03328b1672c77aa01d896808f91cea7ef6bcc
Rost <komarostyslav@gmail.com>
2024-05-28 22:03:44 +0300

    settings.py

--- core/settings.py
+++ core/settings.py
@@ -111,3 +111,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
