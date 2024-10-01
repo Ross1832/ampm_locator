8c0c86063bbf5c1e90da1aed7629a3fe8ad72a66
Ross <komarostyslav@gmail.com>
2024-10-01 21:29:08 +0000

    group_by

--- collector/models.py
+++ collector/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
+class Info(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='info_images/', null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('info_detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
 
 class UsefulInfo(models.Model):
     title = models.CharField(max_length=200)
--- collector/urls.py
+++ collector/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="info"),
+    path('info/', views.info_list, name='info_list'),
 ]
